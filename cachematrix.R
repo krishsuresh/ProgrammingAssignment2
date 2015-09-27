930c03fb5e9a6e293844d6205c9c13eb73c63f4c
krishsuresh <krishsuresh@users.noreply.github.com>
2015-09-28 04:52:24 +0530

    ProgAssi2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R Programming
+## Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+## Assignment: Caching the Inverse of a Matrix
+## Create a cache matrix object to calculate and cache
+## the inverse of a matrix and retrieve as required.
+
+## Usage:
+## M <- matrix(c(1, 2, 3, 4), nrow=2, ncol=2)
+## cacheMatrix <- makeCacheMatrix(M)
+## cacheSolve(cacheMatrix)
+##
+## cacheMatrix$set(M) # Change the matrix being cached.
+## M <- cacheMatrix$get() # Returns the matrix being cached.
+##
+## cacheMatrix$setInvr(solve(data, ...)) # Private function containing cached inverse of x
+## cacheMatrix$getInvr() # Private function used to get the cached inverse of x
+
+## Create a cacheMatrix object for an invertale matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cacheInv <- NULL
+  set <- function(y) {
+    x <<- y
+    cacheInv <<- NULL
+  }
+  get <- function() x
+  setInvr <- function(inverse) cacheInv <<- inverse
+  getInvr <- function() cacheInv
+  list(set = set, get = get,
+       setInvr = setInvr,
+       getInvr = getInvr)
 }
 
 
-## Write a short comment describing this function
+## Return the inverse of an cacheMatrix object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invFunc <- x$getInvr()
+  if(!is.null(invFunc)) {
+    message("getting cached data")
+    return(invFunc)
+  }
+  data <- x$get()
+  invFunc <- solve(data, ...)
+  x$setInvr(invFunc)
+  invFunc
 }
